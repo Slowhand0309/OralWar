6219d2d9fe3afee2f573f9b52e4c7076b418c9f0
slowhand0309 <slowhand0309@gmail.com>
2015-08-27 00:10:23 +0900

    Added blank project

---
+++ OralWar/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  OralWar
+//
+//  Created by MrSmall on 2015/08/26.
+//  Copyright (c) 2015年 MrSmall. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMoveToView(view: SKView) {
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # OralWar
-IOS game application to increase the bacteria!?
+iOS game application to increase the bacteria!?
+
+### Description
+---
+This game is a game that burrows the teeth yourself a fungus.<br />
+Let we are digging to take a fellow of bacteria and items.
+:smiling_imp:
+
+
+### Installation
+---
+
+### Usage
+---
