42f6b613265c037d5c794f96570860568838132a
slowhand0309 <slowhand0309@gmail.com>
2015-08-28 00:31:15 +0900

    Added MenuViewController

---
+++ OralWar/MenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  OralWar
+//
+//  Created by MrSmall on 2015/08/26.
+//  Copyright (c) 2015年 MrSmall. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MenuViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
