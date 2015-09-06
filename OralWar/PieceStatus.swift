2c9f53202e2755626fde8dd927d74c01247d59b4
slowhand0309 <slowhand0309@gmail.com>
2015-09-07 00:51:47 +0900

    Added Method's

--- OralWar/PieceStatus.swift
+++ OralWar/PieceStatus.swift
@@ -21,7 +21,25 @@ public class PieceStatus {
     
     // get being bacteria
     internal func hasBacteria() -> Bool {
-      var tmp = (status & 0xff000000) >> 28
+      var tmp = (status & 0xff000000) >> 24
       return tmp == 1
+    }
+    
+    // get tooth hardness
+    internal func getHardness() -> UInt32 {
+      var tmp = (status & 0x00ff0000) >> 16
+      return tmp
+    }
+    
+    // get item
+    internal func getItem() -> UInt32 {
+      var tmp = (status & 0x0000ff00) >> 8
+      return tmp
+    }
+    
+    // get fellow bacteria
+    internal func getFellow() -> UInt32 {
+      var tmp = (status & 0x000000ff)
+      return tmp
     }
 }
