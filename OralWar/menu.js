d6293df0cfeaf1879aee435704855d22a681637b
slowhand0309 <slowhand0309@gmail.com>
2015-09-12 09:45:11 +0900

    Added menu list

--- OralWar/menu.js
+++ OralWar/menu.js
@@ -1,7 +1,15 @@
 var menuApp = angular.module('menuApp', []);
 
 menuApp.controller('MenuListCtrl', function ($scope) {
-
+  $scope.menus = [
+    {'name': 'ゲームスタート',
+     'describe': 'ゲームを開始'},
+    {'name': '菌リスト',
+     'describe': '集めた菌を見る'},
+    {'name': 'アイテムリスト',
+     'describe': '集めたアイテムを見る'}
+  ];
+                   
   // function onClick
   $scope.onClick = function() {
     location.href = "http://www.google.com";
