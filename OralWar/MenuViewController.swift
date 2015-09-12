4809b963490e1588462e893dfc5bb19dba95b548
slowhand0309 <slowhand0309@gmail.com>
2015-09-12 23:03:49 +0900

    Added List pages

--- OralWar/MenuViewController.swift
+++ OralWar/MenuViewController.swift
@@ -9,25 +9,58 @@
 import UIKit
 import SpriteKit
 
+let ORAL_SCHEME = "oral://view/"
+
+let MENU = "menu"
+let GAMESTART = "gamestart"
+let BACTERIA_LIST = "bacterialist"
+let ITEM_LIST = "itemlist"
+
+let GAME_VIEW_URL = ORAL_SCHEME + GAMESTART
+let BACTERIA_LIST_URL = ORAL_SCHEME + BACTERIA_LIST
+let ITEM_LIST_URL = ORAL_SCHEME + ITEM_LIST
+
 class MenuViewController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var webView: UIWebView!
-    var url = NSBundle.mainBundle().pathForResource("menu", ofType: "html");
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let reqURL = NSURL(string: url!)
-        let req = NSURLRequest(URL: reqURL!)
-        webView.loadRequest(req)
+        locationChanged(MENU)
     }
     
     // handling event
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         
-        println(request)
-        println(navigationType)
+        let url = request.URL!.absoluteString
+        if url!.hasPrefix(ORAL_SCHEME) {
+            
+            if url == GAME_VIEW_URL {
+                startGame()
+            } else if url == BACTERIA_LIST_URL {
+                locationChanged(BACTERIA_LIST)
+            } else if url == ITEM_LIST_URL {
+                locationChanged(ITEM_LIST)
+            }
+            return false
+        }
         return true
+    }
+
+    // Screen transition GameViewController.
+    private func startGame() {
+        let gameViewController = self.storyboard!
+            .instantiateViewControllerWithIdentifier("gameview") as! UIViewController
+        self.presentViewController(gameViewController, animated: false, completion: nil)
+    }
+    
+    // location change
+    private func locationChanged(res :String) {
+        var url = NSBundle.mainBundle().pathForResource(res, ofType: "html");
+        let reqURL = NSURL(string: url!)
+        let req = NSURLRequest(URL: reqURL!)
+        webView.loadRequest(req)
     }
     
     override func shouldAutorotate() -> Bool {
--- OralWar/menu.js
+++ OralWar/menu.js
@@ -2,16 +2,20 @@ var menuApp = angular.module('menuApp', []);
 
 menuApp.controller('MenuListCtrl', function ($scope) {
   $scope.menus = [
-    {'name': 'ゲームスタート',
+    {'id' : 'gamestart',
+     'name': 'ゲームスタート',
      'describe': 'ゲームを開始'},
-    {'name': '菌リスト',
+    {'id' : 'bacterialist',
+     'name': '菌リスト',
      'describe': '集めた菌を見る'},
-    {'name': 'アイテムリスト',
+    {'id' : 'itemlist',
+     'name': 'アイテムリスト',
      'describe': '集めたアイテムを見る'}
   ];
                    
   // function onClick
-  $scope.onClick = function() {
-    location.href = "http://www.google.com";
+  $scope.onClick = function(id) {
+    var url = "oral://view/" + id
+    location.href = url;
   };
 });
