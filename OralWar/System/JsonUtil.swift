2bad453783d22d5b8d4807b5d4250302091e9cde
slowhand0309 <slowhand0309@gmail.com>
2015-09-30 23:13:19 +0900

    Added JsonUtil.swift

---
+++ OralWar/System/JsonUtil.swift
@@ -0,0 +1,52 @@
+//
+//  JsonUtil.swift
+//  OralWar
+//
+//  Created by MrSmall on 2015/09/30.
+//  Copyright © 2015年 MrSmall. All rights reserved.
+//
+
+import Foundation
+
+let TYPE: String = "json"
+
+// extension : add isEmpty func
+extension NSData {
+    
+    func isEmpty() -> Bool {
+        return self.length == 0
+    }
+}
+
+class JsonUtil {
+    
+    var jsonData: NSDictionary!
+    
+    init () {
+        
+    }
+    
+    // get json data from resource
+    func getJsonDataFromResource(fileName: String) -> NSData {
+        
+        guard let path = NSBundle.mainBundle().pathForResource(fileName, ofType: TYPE) else {
+            return NSData()
+        }
+        guard let fileHandle: NSFileHandle = NSFileHandle(forReadingAtPath: path) else {
+            return NSData()
+        }
+        return fileHandle.readDataToEndOfFile()
+    }
+    
+    // parse json data
+    func parse(data: NSData) -> Bool {
+        
+        do {
+            jsonData = try NSJSONSerialization.JSONObjectWithData(
+                data, options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
+        } catch  {
+            return false
+        }
+        return true
+    }
+}
