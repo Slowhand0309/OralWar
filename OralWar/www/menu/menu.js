0f964cbbaa9dbed272d535a9a26d9ca75c51294a
slowhand0309 <slowhand0309@gmail.com>
2015-10-28 01:22:48 +0900

    Updated menu background

--- OralWar/www/menu/menu.js
+++ OralWar/www/menu/menu.js
@@ -2,12 +2,12 @@ var menuApp = angular.module('menuApp', []);
 
 menuApp.controller('MenuListCtrl', function ($scope) {
   $scope.menus = [
-    {'id' : 'gamestart',
-     'name': 'ゲームスタート',
-     'describe': 'ゲームを開始'},
     {'id' : 'bacterialist',
      'name': '菌リスト',
      'describe': '集めた菌を見る'},
+    {'id' : 'gamestart',
+     'name': 'ゲームスタート',
+     'describe': 'ゲームを開始'},
     {'id' : 'itemlist',
      'name': 'アイテムリスト',
      'describe': '集めたアイテムを見る'}
