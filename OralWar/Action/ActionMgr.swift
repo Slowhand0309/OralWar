92c36262892220f205149df67433bc41a8792648
slowhand0309 <slowhand0309@gmail.com>
2015-11-16 23:21:26 +0900

    Updated ActionMgr.swift

--- OralWar/Action/ActionMgr.swift
+++ OralWar/Action/ActionMgr.swift
@@ -11,5 +11,23 @@ import SpriteKit
 
 // SKAction manager class
 class ActionMgr {
-    
+ 
+    // create action for bacteria
+    func createAction(type: AnimationType) -> ActionBase? {
+        var action: ActionBase?
+        switch type {
+        case AnimationType.STAY_TYPE: // stay here
+            break
+        case AnimationType.FRONT_BACK_TYPE: // move front back
+            break
+        case AnimationType.LEFT_RIGHT_TYPE: // move left right
+            break
+        case AnimationType.CIRCLE_TYPE: // move circle
+            action = ActionRotation(isRight: true)
+            break
+        case AnimationType.RANDAM_TYPE: // move randam
+            break
+        }
+        return action
+    }
 }
